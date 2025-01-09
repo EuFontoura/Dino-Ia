96dcfab4e66d4430208274d660c09c8219e68738
EuFontoura <gabrielFMendonca@gmail.com>
2025-01-09 09:20:20 -0300

    :lipstick: feat: Hitbox e linhas de detecção

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 import pygame
-import neat
 import os
 import random
-import sys
 import math
+import sys
+import neat
 
 pygame.init()
 
-# Constantes
+# Constantes Globais
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 RUNNING = [pygame.image.load(os.path.join("Assets/Dino", "DinoRun1.png")),
-                    pygame.image.load(os.path.join("Assets/Dino", "DinoRun2.png"))]
+           pygame.image.load(os.path.join("Assets/Dino", "DinoRun2.png"))]
 
 JUMPING = pygame.image.load(os.path.join("Assets/Dino", "DinoJump.png"))
 
 SMALL_CACTUS = [pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus1.png")),
-                                pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus2.png")),
-                                pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus3.png")),]
+                pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus2.png")),
+                pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus3.png"))]
 LARGE_CACTUS = [pygame.image.load(os.path.join("Assets/Cactus", "LargeCactus1.png")),
-                                pygame.image.load(os.path.join("Assets/Cactus", "LargeCactus2.png")),
-                                pygame.image.load(os.path.join("Assets/Cactus", "LargeCactus3.png")),]
+                pygame.image.load(os.path.join("Assets/Cactus", "LargeCactus2.png")),
+                pygame.image.load(os.path.join("Assets/Cactus", "LargeCactus3.png"))]
 
 BG = pygame.image.load(os.path.join("Assets/Other", "Track.png"))
 
 FONT = pygame.font.Font('freesansbold.ttf', 20)
+
 
 class Dinosaur:
     X_POS = 80
@@ -38,7 +39,8 @@ class Dinosaur:
         self.dino_run = True
         self.dino_jump = False
         self.jump_vel = self.JUMP_VEL
-        self.rect = pygame.Rect(self.X_POS, self. Y_POS, img.get_width(), img.get_height())
+        self.rect = pygame.Rect(self.X_POS, self.Y_POS, img.get_width(), img.get_height())
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         self.step_index = 0
 
     def update(self):
@@ -67,6 +69,10 @@ class Dinosaur:
 
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.rect.x, self.rect.y))
+        pygame.draw.rect(SCREEN, self.color, (self.rect.x, self.rect.y, self.rect.width, self.rect.height), 2)
+        for obstacle in obstacles:
+            pygame.draw.line(SCREEN, self.color, (self.rect.x + 54, self.rect.y + 12), obstacle.rect.center, 2)
+
 
 class Obstacle:
     def __init__(self, image, number_of_cacti):
@@ -74,34 +80,39 @@ class Obstacle:
         self.type = number_of_cacti
         self.rect = self.image[self.type].get_rect()
         self.rect.x = SCREEN_WIDTH
-    
+
     def update(self):
         self.rect.x -= game_speed
-        if self.rect.x < self.rect.width:
+        if self.rect.x < -self.rect.width:
             obstacles.pop()
 
     def draw(self, SCREEN):
         SCREEN.blit(self.image[self.type], self.rect)
+
 
 class SmallCactus(Obstacle):
     def __init__(self, image, number_of_cacti):
         super().__init__(image, number_of_cacti)
         self.rect.y = 325
 
+
 class LargeCactus(Obstacle):
     def __init__(self, image, number_of_cacti):
         super().__init__(image, number_of_cacti)
         self.rect.y = 300
+
 
 def remove(index):
     dinosaurs.pop(index)
     ge.pop(index)
     nets.pop(index)
 
+
 def distance(pos_a, pos_b):
     dx = pos_a[0]-pos_b[0]
     dy = pos_a[1]-pos_b[1]
     return math.sqrt(dx**2+dy**2)
+
 
 def eval_genomes(genomes, config):
     global game_speed, x_pos_bg, y_pos_bg, obstacles, dinosaurs, ge, nets, points
@@ -128,16 +139,26 @@ def eval_genomes(genomes, config):
         global points, game_speed
         points += 1
         if points % 100 == 0:
-                game_speed += 1
-        text = FONT.render(f'Points: {str(points)}', True, (0, 0, 0))
+            game_speed += 1
+        text = FONT.render(f'Points:  {str(points)}', True, (0, 0, 0))
         SCREEN.blit(text, (950, 50))
+
+    def statistics():
+        global dinosaurs, game_speed, ge
+        text_1 = FONT.render(f'Dinosaurs Alive:  {str(len(dinosaurs))}', True, (0, 0, 0))
+        text_2 = FONT.render(f'Generation:  {pop.generation+1}', True, (0, 0, 0))
+        text_3 = FONT.render(f'Game Speed:  {str(game_speed)}', True, (0, 0, 0))
+
+        SCREEN.blit(text_1, (50, 450))
+        SCREEN.blit(text_2, (50, 480))
+        SCREEN.blit(text_3, (50, 510))
 
     def background():
         global x_pos_bg, y_pos_bg
         image_width = BG.get_width()
         SCREEN.blit(BG, (x_pos_bg, y_pos_bg))
         SCREEN.blit(BG, (image_width + x_pos_bg, y_pos_bg))
-        if x_pos_bg <= image_width:
+        if x_pos_bg <= -image_width:
             x_pos_bg = 0
         x_pos_bg -= game_speed
 
@@ -149,20 +170,20 @@ def eval_genomes(genomes, config):
                 sys.exit()
 
         SCREEN.fill((255, 255, 255))
-        
+
         for dinosaur in dinosaurs:
             dinosaur.update()
             dinosaur.draw(SCREEN)
 
-            if len(dinosaurs) == 0:
-                break
+        if len(dinosaurs) == 0:
+            break
 
-            if len(obstacles) == 0:
-                rand_int = random.randint(0, 1)
-                if rand_int == 0:
-                    obstacles.append(SmallCactus(SMALL_CACTUS, random.randint (0, 2)))
-                elif rand_int == 1:
-                    obstacles.append(LargeCactus(LARGE_CACTUS, random.randint(0, 2)))
+        if len(obstacles) == 0:
+            rand_int = random.randint(0, 1)
+            if rand_int == 0:
+                obstacles.append(SmallCactus(SMALL_CACTUS, random.randint(0, 2)))
+            elif rand_int == 1:
+                obstacles.append(LargeCactus(LARGE_CACTUS, random.randint(0, 2)))
 
         for obstacle in obstacles:
             obstacle.draw(SCREEN)
@@ -180,14 +201,16 @@ def eval_genomes(genomes, config):
                 dinosaur.dino_jump = True
                 dinosaur.dino_run = False
 
+        statistics()
         score()
         background()
         clock.tick(30)
         pygame.display.update()
 
 
-# Configurando NEAT 
+# Configurando NEAT
 def run(config_path):
+    global pop
     config = neat.config.Config(
         neat.DefaultGenome,
         neat.DefaultReproduction,
@@ -198,6 +221,7 @@ def run(config_path):
 
     pop = neat.Population(config)
     pop.run(eval_genomes, 50)
+
 
 if __name__ == '__main__':
     local_dir = os.path.dirname(__file__)
